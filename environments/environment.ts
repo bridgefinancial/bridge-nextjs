bc5a03cce443c6680cee9da00c4ce7745a7d8b1e
Garrison Whitaker <92746066+whitagar@users.noreply.github.com>
2024-09-11 16:21:03 -0600

    Use next public env var (#26)
    Co-authored-by: Garrison Whitaker <gar@Lexis-MacBook-Pro.local>

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  DJANGO_API_BASE_URL: process.env['DJANGO_API_BASE_URL'] ?? 'http://localhost:8000',
+  DJANGO_API_BASE_URL:
+    process.env["NEXT_PUBLIC_DJANGO_API_BASE_URL"] ?? "http://localhost:8000",
 };

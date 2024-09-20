6b54afbccca3eed08af749cb28ce355d4bc88846
landonwjohnson <landonwjohnson@gmail.com>
2024-09-20 16:01:30 -0600

    reverted form

--- src/components/organisms/forms/Form.tsx
+++ src/components/organisms/forms/Form.tsx
@@ -1,11 +1,10 @@
 import { useQuestionnaire } from "@/providers/Questionnaire.provider";
-import React, { useEffect, useRef, forwardRef } from "react";
-import ReactDOM from "react-dom";
+import React, { forwardRef } from "react";
 import FormPage from "./FormPage";
 import FormAction, { FormActionConfig } from "./FormAction";
 import clsx from "clsx";
+import { environment } from "../../../../environments/environment";
 import LoadingSpinner from "@/components/atoms/loaders/LoadingSpinner";
-import { Box } from "@mui/material";
 
 type FormProps = {
   previousButtonConfig: FormActionConfig;
@@ -13,79 +12,7 @@ type FormProps = {
   submitButtonConfig: FormActionConfig;
 };
 
-interface FormFooterProps extends FormProps {
-  pageIndex: number;
-  form: any; // Adjust this type based on the structure of the form object
-}
 
-const FormFooter = (props: FormFooterProps) => {
-  const {
-    form,
-    pageIndex,
-    previousButtonConfig,
-    nextButtonConfig,
-    submitButtonConfig,
-  } = props;
-
-  // Create a ref for the portal container div
-  const portalContainerRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    // Create a div element and append it to the document body
-    const portalDiv = document.createElement("div");
-    portalContainerRef.current = portalDiv;
-    document.body.appendChild(portalDiv);
-
-    // Clean up the div element when the component unmounts
-    return () => {
-      if (portalDiv) {
-        document.body.removeChild(portalDiv);
-      }
-    };
-  }, []);
-
-  if (!portalContainerRef.current) {
-    return null;
-  }
-
-  // Render the FormFooter via a portal
-  return ReactDOM.createPortal(
-    <Box sx={{ position: "fixed", bottom: 0, width: "100%" }}>
-      <div className="w-full absolute bottom-0">
-        {/* Progress */}
-        <div className="w-full flex items-center justify-center gap-2">
-          {form.definition.pages.map((_: any, index: number) => {
-            return (
-              <div
-                key={`page-indicator-${index}`}
-                className={clsx("basis-0 shrink grow rounded-full h-2", {
-                  "bg-bridge-dark-purple": index < pageIndex,
-                  "bg-gray-300": index >= pageIndex,
-                })}
-              />
-            );
-          })}
-        </div>
-        <Box>
-          <div
-           
-            className="w-full flex items-center justify-between bg-white py-6 px-16"
-          >
-            {/* Previous */}
-            <FormAction {...previousButtonConfig} />
-
-            {/* Next Page */}
-            <FormAction {...nextButtonConfig} />
-
-            {/* Submit */}
-            <FormAction {...submitButtonConfig} />
-          </div>
-        </Box>
-      </div>
-    </Box>,
-    portalContainerRef.current // Dynamically created portal container
-  );
-};
 
 const Form = forwardRef(
   (
@@ -135,14 +62,32 @@ const Form = forwardRef(
           )}
         </div>
 
-        {/* Footer is now rendered through a portal */}
-        <FormFooter
-          previousButtonConfig={previousButtonConfig}
-          nextButtonConfig={nextButtonConfig}
-          submitButtonConfig={submitButtonConfig}
-          form={form}
-          pageIndex={pageIndex}
-        />
+        <div className="w-full absolute bottom-0">
+          {/* Progress */}
+          <div className="w-full flex items-center justify-center gap-2">
+            {form.definition.pages.map((_, index) => {
+              return (
+                <div
+                  key={`page-indicator-${index}`}
+                  className={clsx("basis-0 shrink grow rounded-full h-2", {
+                    "bg-bridge-dark-purple": index < pageIndex,
+                    "bg-gray-300": index >= pageIndex,
+                  })}
+                ></div>
+              );
+            })}
+          </div>
+          <div className="w-full flex items-center justify-between bg-white py-6 px-16">
+            {/* Previous */}
+            <FormAction {...previousButtonConfig} />
+
+            {/* Next Page */}
+            <FormAction {...nextButtonConfig} />
+
+            {/* Submit */}
+            <FormAction {...submitButtonConfig} />
+          </div>
+        </div>
       </form>
     );
   }

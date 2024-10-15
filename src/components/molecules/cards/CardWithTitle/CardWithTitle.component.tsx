95b24ecd2867ce2559b1d947a244cf0f8c7b4a22
landonwjohnson <landonwjohnson@gmail.com>
2024-10-14 19:26:54 -0600

    fix: add use client to verify email sent and updated use breakpoint query hook

--- src/components/molecules/cards/CardWithTitle/CardWithTitle.component.tsx
+++ src/components/molecules/cards/CardWithTitle/CardWithTitle.component.tsx
@@ -1,9 +1,9 @@
-import TitleText from "@/components/atoms/typography/TitleText";
-import { useBreakpointQuery } from "@/hooks/useBreakpointQuery.hook";
-import { BaseTypographyProps } from "@/types/base-typography-props.interface";
-import { Box } from "@mui/material";
-import merge from "lodash.merge"; // Import lodash's merge utility
-import React, { FC, useMemo } from "react";
+import TitleText from '@/components/atoms/typography/TitleText';
+import { useBreakpointQuery } from '@/hooks/useBreakpointQuery.hook';
+import { BaseTypographyProps } from '@/types/base-typography-props.interface';
+import { Box } from '@mui/material';
+import merge from 'lodash.merge'; // Import lodash's merge utility
+import React, { FC, useMemo } from 'react';
 
 interface TitleProps extends BaseTypographyProps {
   text: string;
@@ -20,20 +20,25 @@ const CardWithTitle: FC<CardWithTitleProps> = (props: CardWithTitleProps) => {
   const { sx = {}, children, titleProps } = props;
   const { containerStyle } = props;
   const { text, ...titleTextProps } = titleProps;
-  const { matches: isMobile, loading } = useBreakpointQuery({
-    minWidth: 280,
-    maxWidth: 380,
-  });
+  const { matches: isMobile, loading } = useBreakpointQuery(
+    {
+      minWidth: 280,
+      maxWidth: 380,
+    },
+    {
+      noSsr: true,
+    }
+  );
 
   // Define default styles for the outer Box component
   const defaultOuterStyles = useMemo(
     () => ({
       borderRadius: 4,
       maxWidth: 580,
-      margin: "auto",
+      margin: 'auto',
       mt: 4,
     }),
-    [],
+    []
   );
 
   // Define default styles for the TitleText
@@ -41,18 +46,18 @@ const CardWithTitle: FC<CardWithTitleProps> = (props: CardWithTitleProps) => {
     () => ({
       fontSize: isMobile ? 25 : 32,
     }),
-    [isMobile],
+    [isMobile]
   );
 
   // Merge default styles with custom styles
   const mergedOuterStyles = useMemo(
     () => merge(containerStyle, defaultOuterStyles, sx),
-    [sx, defaultOuterStyles, containerStyle],
+    [sx, defaultOuterStyles, containerStyle]
   );
 
   const mergedTextTitleStyles = useMemo(
     () => merge(defaultTitleStyles, titleTextProps.sx || {}),
-    [defaultTitleStyles, titleTextProps.sx],
+    [defaultTitleStyles, titleTextProps.sx]
   );
 
   return (
@@ -67,10 +72,10 @@ const CardWithTitle: FC<CardWithTitleProps> = (props: CardWithTitleProps) => {
       >
         {text ? (
           <TitleText
-            variant={titleTextProps.variant || "h1"}
-            component={titleTextProps.component || "h1"}
+            variant={titleTextProps.variant || 'h1'}
+            component={titleTextProps.component || 'h1'}
             gutterBottom={true}
-            textAlign={titleTextProps.textAlign || "center"}
+            textAlign={titleTextProps.textAlign || 'center'}
             sx={mergedTextTitleStyles}
           >
             {text}

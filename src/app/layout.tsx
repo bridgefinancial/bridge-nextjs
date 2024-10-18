1ef95343b1ecd3e7e5b1d19c9838702b345e8669
Garrison Whitaker <gar@Lexis-MacBook-Pro.local>
2024-10-17 21:08:45 -0600

    Fix styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-"use client";
+'use client';
 
-import MainProvider from "@/providers/Main.provider";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { Inter } from "next/font/google";
-import Head from "next/head"; // Import Head from next/head
-import "../scss/defaults.scss";
-import "../scss/material-theme.scss";
-import "../scss/mixins.scss";
-import "../scss/open-color.scss";
-import "../scss/typography.scss";
-import "../scss/variables.scss";
-import theme from "../theme/theme"; // Ensure this is a client-side import
-import "./globals.css";
-import Providers from "./providers";
+import MainProvider from '@/providers/Main.provider';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Inter } from 'next/font/google';
+import Head from 'next/head'; // Import Head from next/head
+import '../app/globals.css';
+import '../scss/defaults.scss';
+import '../scss/material-theme.scss';
+import '../scss/mixins.scss';
+import '../scss/open-color.scss';
+import '../scss/typography.scss';
+import '../scss/variables.scss';
+import theme from '../theme/theme'; // Ensure this is a client-side import
+import Providers from './providers';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,

e17f64e47a132830ad08699b56fa6a04c5c751d7
Garrison Whitaker <garrisonkwhitaker@gmail.com>
2024-11-06 18:06:52 -0700

    Add comment

--- src/services/forms.service.ts
+++ src/services/forms.service.ts
@@ -2,6 +2,7 @@ import { FormidableForm } from '@/types/forms.types';
 import { baseUrls } from '@/utils/env-variables';
 import { useQueries, useQuery, UseQueryOptions } from '@tanstack/react-query';
 
+// Use static django json files
 const BASE_URL =
   process.env.NEXT_PUBLIC_DJANGO_API_BASE_URL ?? baseUrls.api.full;
 

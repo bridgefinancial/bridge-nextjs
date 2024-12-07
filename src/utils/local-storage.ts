506c8c7633cdfc05d1a119a5e86eaad574d12295
Sean Park <sjpsean@gmail.com>
2024-12-07 08:51:34 -0700

    added clearQuestionnaireData function to local storage utils

--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -1,3 +1,14 @@
 export const getLandingConfigKey = (formId: number) => {
   return `last-viewed-config-form-id-${formId}`;
 };
+
+export const clearQuestionnaireData = () => {
+  Object.keys(localStorage).forEach((key) => {
+    if (
+      key.startsWith('questionnaire-responses-') ||
+      key.startsWith('last-viewed-config-form-id')
+    ) {
+      localStorage.removeItem(key); // Remove the key
+    }
+  });
+};

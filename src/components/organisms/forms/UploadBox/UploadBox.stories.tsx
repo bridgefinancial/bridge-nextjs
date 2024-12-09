15cc5ae2bba06e4f6136952f40f642e0f9d1f0a9
landonwjohnson <landonwjohnson@gmail.com>
2024-12-09 16:50:44 -0700

    drag and drop works with stories

--- src/components/organisms/forms/UploadBox/UploadBox.stories.tsx
+++ src/components/organisms/forms/UploadBox/UploadBox.stories.tsx
@@ -45,12 +45,12 @@ export default meta;
 
 type Story = StoryObj<typeof UploadBox>;
 
-const formatFileSize = (size: number) => `${(size / 1024).toFixed(2)} KB`;
 
 export const DragAndDropWithStatus: Story = {
   render: () => {
     const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
     const [fileStatuses, setFileStatuses] = useState<string[]>([]);
+    const formatFileSize = (size: number) => `${(size / 1024).toFixed(2)} KB`;
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       if (event.target.files) {
@@ -124,41 +124,3 @@ export const DragAndDropWithStatus: Story = {
   },
 };
 
-export const WithFileRemoval: Story = {
-  render: () => {
-    const [files, setFiles] = useState<File[]>([]);
-
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (event.target.files) {
-        const newFiles = Array.from(event.target.files);
-        setFiles((prevFiles) => [...prevFiles, ...newFiles]);
-      }
-    };
-
-    const handleRemoveFile = (index: number) => {
-      setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
-    };
-
-    return (
-      <UploadBox
-        supportedFormats={['pdf', 'docx', 'jpg']}
-        dropzoneText="Drag and drop files to upload and manage them"
-        onFileChange={handleFileChange}
-        onUploadClicked={() => console.log('Upload clicked')}
-        handleRemoveFile={handleRemoveFile}
-      >
-        <div>
-          <h3>File List:</h3>
-          <ul>
-            {files.map((file, index) => (
-              <li key={index}>
-                {file.name} ({formatFileSize(file.size)}){' '}
-                <button onClick={() => handleRemoveFile(index)}>Remove</button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </UploadBox>
-    );
-  },
-};
